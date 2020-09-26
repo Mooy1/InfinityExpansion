439adcfadf5da10247184f67f2bb0d58e6117576
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2020-09-25 23:48:22 -0500

    getting there...

--- src/main/java/me/mooy/MooyAddon/MooyAddon.java
+++ src/main/java/me/mooy/MooyAddon/MooyAddon.java
@@ -1,5 +1,6 @@
 package me.mooy.mooyaddon;
 
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
@@ -17,39 +18,15 @@ public class MooyAddon extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
-        // Read something from your config.yml
+
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update")) {
             // You could start an Auto-Updater for example
         }
 
-        ItemStack mainCategoryItem = new CustomItem(Material.EMERALD_BLOCK, "&4Mooy Addon", "", "&a> Click to open");
-        NamespacedKey mainCategoryId = new NamespacedKey(this, "mooyaddon_main");
-        Category main = new Category(mainCategoryId, mainCategoryItem);
+        AddItems();
 
-        ItemStack cobble = new ItemStack(Material.COBBLESTONE);
-
-        SlimefunItemStack compressedCobbleItem = new SlimefunItemStack(
-                "COMPRESSED_COBBLESTONE_1",
-                Material.ANDESITE,
-                "&7Compressed Cobblestone",
-                "",
-                "&89 cobblestone in 1"
-        );
-
-        ItemStack[] compressedCobbleRecipe = {
-                cobble, cobble, cobble,
-                cobble, cobble, cobble,
-                cobble, cobble, cobble
-        };
-        SlimefunItem compressedCobble = new SlimefunItem(
-                main,
-                compressedCobbleItem,
-                RecipeType.ENHANCED_CRAFTING_TABLE,
-                compressedCobbleRecipe
-        );
-        compressedCobble.register(this);
     }
 
     @Override
@@ -67,4 +44,201 @@ public class MooyAddon extends JavaPlugin implements SlimefunAddon {
         return this;
     }
 
+    public void AddItems() {
+
+        ItemStack mainCategoryItem = new CustomItem(Material.EMERALD_BLOCK, "&4Mooy Addon", "", "&a> Click to open");
+        NamespacedKey mainCategoryId = new NamespacedKey(this, "mooyaddon_main");
+        Category main = new Category(mainCategoryId, mainCategoryItem);
+
+        ItemStack material1 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_1 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_1",
+                        Material.ANDESITE,
+                        "&71x Compressed Cobblestone",
+                        "",
+                        "&89 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material1, material1, material1,
+                        material1, material1, material1,
+                        material1, material1, material1,
+                }
+        );
+
+        ItemStack material2 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_2 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_2",
+                        Material.ANDESITE,
+                        "&72x Compressed Cobblestone",
+                        "",
+                        "&881 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material2, material2, material2,
+                        material2, material2, material2,
+                        material2, material2, material2,
+                }
+        );
+
+        ItemStack material3 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_3 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_3",
+                        Material.ANDESITE,
+                        "&73x Compressed Cobblestone",
+                        "",
+                        "&8243 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material3, material3, material3,
+                        material3, material3, material3,
+                        material3, material3, material3,
+                }
+        );
+
+        ItemStack material4 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_4 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_4",
+                        Material.ANDESITE,
+                        "&74x Compressed Cobblestone",
+                        "",
+                        "&86,561 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material4, material4, material4,
+                        material4, material4, material4,
+                        material4, material4, material4,
+                }
+        );
+
+        ItemStack material5 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_5 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_5",
+                        Material.ANDESITE,
+                        "&75x Compressed Cobblestone",
+                        "",
+                        "&859,049 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material5, material5, material5,
+                        material5, material5, material5,
+                        material5, material5, material5,
+                }
+        );
+
+        ItemStack material6 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_6 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_6",
+                        Material.ANDESITE,
+                        "&76x Compressed Cobblestone",
+                        "",
+                        "&8531,441 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material6, material6, material6,
+                        material6, material6, material6,
+                        material6, material6, material6,
+                }
+        );
+
+        ItemStack material7 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_7 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_7",
+                        Material.ANDESITE,
+                        "&77x Compressed Cobblestone",
+                        "",
+                        "&84,782,969 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material7, material7, material7,
+                        material7, material7, material7,
+                        material7, material7, material7,
+                }
+        );
+
+        ItemStack material8 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_8 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_8",
+                        Material.ANDESITE,
+                        "&78x Compressed Cobblestone",
+                        "",
+                        "&843,046,721 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material8, material8, material8,
+                        material8, material8, material8,
+                        material8, material8, material8,
+                }
+        );
+
+        ItemStack material9 = new ItemStack(Material.COBBLESTONE);
+
+        SlimefunItem COMPRESSED_COBBLESTONE_9 = new SlimefunItem(
+                main,
+                new SlimefunItemStack(
+                        "COMPRESSED_COBBLESTONE_9",
+                        Material.ANDESITE,
+                        "&79x Compressed Cobblestone",
+                        "",
+                        "&8387,420,489 cobblestone combined",
+                        ""
+                ),
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        material9, material9, material9,
+                        material9, material9, material9,
+                        material9, material9, material9,
+                }
+        );
+
+        COMPRESSED_COBBLESTONE_1.register(this);
+        COMPRESSED_COBBLESTONE_2.register(this);
+        COMPRESSED_COBBLESTONE_3.register(this);
+        COMPRESSED_COBBLESTONE_4.register(this);
+        COMPRESSED_COBBLESTONE_5.register(this);
+        COMPRESSED_COBBLESTONE_6.register(this);
+        COMPRESSED_COBBLESTONE_7.register(this);
+        COMPRESSED_COBBLESTONE_8.register(this);
+        COMPRESSED_COBBLESTONE_9.register(this);
+
+    }
 }
