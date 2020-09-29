1b44780f6ff8f73405cfebdbf8eeafa070262865
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2020-09-29 14:05:21 -0500

    moved cores to magic workbench

--- src/main/java/me/mooy1/infinityexpansion/Materials/Cores.java
+++ src/main/java/me/mooy1/infinityexpansion/Materials/Cores.java
@@ -19,7 +19,7 @@ public class Cores extends SlimefunItem{
     private final Core core;
 
     public Cores(Cores.Core core) {
-        super(Items.MOOYMATERIALS, core.getItem(), core.getRecipetype(), core.getRecipe());
+        super(Items.MOOYMATERIALS, core.getItem(), RecipeType.MAGIC_WORKBENCH, core.getRecipe());
         this.core = core;
     }
 
@@ -27,15 +27,15 @@ public class Cores extends SlimefunItem{
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public enum Core {
 
-        MAGNESIUM(Items.MAGNESIUM_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.MAGNESIUM_INGOT, 64))),
-        COPPER(Items.COPPER_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.COPPER_INGOT, 64))),
-        SILVER(Items.SILVER_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.SILVER_INGOT, 64))),
-        ALUMINUM(Items.ALUMINUM_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.ALUMINUM_INGOT, 64))),
-        LEAD(Items.LEAD_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.LEAD_INGOT, 64))),
-        ZINC(Items.ZINC_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.ZINC_INGOT, 64))),
-        TIN(Items.TIN_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new SlimefunItemStack(SlimefunItems.TIN_INGOT, 64))),
-        IRON(Items.IRON_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.IRON_INGOT, 64))),
-        GOLD(Items.GOLD_CORE, RecipeType.ENHANCED_CRAFTING_TABLE,
+        MAGNESIUM(Items.MAGNESIUM_CORE, Compress(new SlimefunItemStack(SlimefunItems.MAGNESIUM_INGOT, 64))),
+        COPPER(Items.COPPER_CORE, Compress(new SlimefunItemStack(SlimefunItems.COPPER_INGOT, 64))),
+        SILVER(Items.SILVER_CORE, Compress(new SlimefunItemStack(SlimefunItems.SILVER_INGOT, 64))),
+        ALUMINUM(Items.ALUMINUM_CORE, Compress(new SlimefunItemStack(SlimefunItems.ALUMINUM_INGOT, 64))),
+        LEAD(Items.LEAD_CORE, Compress(new SlimefunItemStack(SlimefunItems.LEAD_INGOT, 64))),
+        ZINC(Items.ZINC_CORE, Compress(new SlimefunItemStack(SlimefunItems.ZINC_INGOT, 64))),
+        TIN(Items.TIN_CORE, Compress(new SlimefunItemStack(SlimefunItems.TIN_INGOT, 64))),
+        IRON(Items.IRON_CORE, Compress(new ItemStack(Material.IRON_INGOT, 64))),
+        GOLD(Items.GOLD_CORE,
                 new ItemStack[] {
                         new ItemStack(Material.GOLD_INGOT, 64),
                         new SlimefunItemStack(SlimefunItems.GOLD_4K, 64),
@@ -47,16 +47,15 @@ public class Cores extends SlimefunItem{
                         new SlimefunItemStack(SlimefunItems.GOLD_4K, 64),
                         new ItemStack(Material.GOLD_INGOT, 64),
                 }),
-        NETHERITE(Items.NETHERITE_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.NETHERITE_INGOT, 16))),
-        DIAMOND(Items.DIAMOND_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.DIAMOND, 32))),
-        EMERALD(Items.EMERALD_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.EMERALD, 32))),
-        COAL(Items.COAL_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.COAL, 64))),
-        LAPIS(Items.LAPIS_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.LAPIS_BLOCK, 64))),
-        REDSTONE(Items.REDSTONE_CORE, RecipeType.ENHANCED_CRAFTING_TABLE, Compress(new ItemStack(Material.REDSTONE_BLOCK, 64)));
+        NETHERITE(Items.NETHERITE_CORE, Compress(new ItemStack(Material.NETHERITE_INGOT, 16))),
+        DIAMOND(Items.DIAMOND_CORE, Compress(new ItemStack(Material.DIAMOND, 32))),
+        EMERALD(Items.EMERALD_CORE, Compress(new ItemStack(Material.EMERALD, 32))),
+        COAL(Items.COAL_CORE, Compress(new ItemStack(Material.COAL, 64))),
+        LAPIS(Items.LAPIS_CORE, Compress(new ItemStack(Material.LAPIS_BLOCK, 64))),
+        REDSTONE(Items.REDSTONE_CORE, Compress(new ItemStack(Material.REDSTONE_BLOCK, 64)));
 
         @Nonnull
         private final SlimefunItemStack item;
-        private final RecipeType recipetype;
         private final ItemStack[] recipe;
 
     }
