fa03505324ab0cff094901e70dc776e86400324b
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2020-09-30 11:09:43 -0500

    [CI Skip] oop

--- src/main/java/me/mooy1/infinityexpansion/Machines/Quarry.java
+++ src/main/java/me/mooy1/infinityexpansion/Machines/Quarry.java
@@ -32,8 +32,6 @@ public class Quarry extends SlimefunItem implements InventoryBlock, EnergyNetCom
 
     RandomizedSet<ItemStack> randomizer = new RandomizedSet<>();
 
-    randomizer.add()
-
     private final Quarry.Tier tier;
     private final int[] OUTPUTSLOTS = new int[] {28, 29, 30, 31, 32, 33, 34, 37, 38, 39, 40, 41, 42, 43};
 
