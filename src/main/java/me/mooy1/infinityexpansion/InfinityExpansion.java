d8feb04411f4abab837e71269afc169dce84e6e5
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2020-10-01 01:30:36 -0500

    Testing auto updater...

--- src/main/java/me/mooy1/infinityexpansion/InfinityExpansion.java
+++ src/main/java/me/mooy1/infinityexpansion/InfinityExpansion.java
@@ -4,11 +4,13 @@ import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import me.mooy1.infinityexpansion.materials.VoidDustResource;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
-
+import me.mrCookieSlime.Slimefun.cscorelib2.updater.Updater;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
+import java.util.logging.Level;
 
 public class InfinityExpansion extends JavaPlugin implements SlimefunAddon {
 
@@ -28,8 +30,19 @@ public class InfinityExpansion extends JavaPlugin implements SlimefunAddon {
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
-            new GitHubBuildsUpdater(this, getFile(), "Mooy1/InfinityExpansion/master").start();
+            Updater updater = new GitHubBuildsUpdater(this, this.getFile(), "Mooy1/InfinityExpansion/master");
+            updater.start();
+        } else {
+            if (!cfg.getBoolean("options.auto-update")) {
+                getLogger().log(Level.WARNING, "Enable auto updates in the config to get the latest builds!");
+            }
+            if (!getDescription().getVersion().startsWith("DEV - ")){
+                getLogger().log(Level.INFO, "You must be on a DEV build to auto update!");
+                getLogger().log(Level.INFO, "Current version: " + getDescription().getVersion());
+            }
         }
+
+
 
         //Register items
 
